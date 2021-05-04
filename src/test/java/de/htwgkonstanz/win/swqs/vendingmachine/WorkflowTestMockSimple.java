a67029d44d6f219b99d93b693916da0acc9b4617
Markus Eiglsperger <meiglspe@htwg-konstanz.de>
2021-05-04 18:07:53 +0200

    Removed remaining links to JUnit 4.

--- src/test/java/de/htwgkonstanz/win/swqs/vendingmachine/WorkflowTestMockSimple.java
+++ src/test/java/de/htwgkonstanz/win/swqs/vendingmachine/WorkflowTestMockSimple.java
@@ -1,10 +1,10 @@
 package de.htwgkonstanz.win.swqs.vendingmachine;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class WorkflowTestMockSimple {
